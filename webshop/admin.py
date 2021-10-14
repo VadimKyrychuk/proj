8ca7597475cea16310af7f7ada05e0c0d67dfe64
Vodem <vadkirichuk@gmail.com>
2021-10-14 13:39:44 +0300

    'modelAdmin'

--- webshop/admin.py
+++ webshop/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import *
+from django import forms
 
-# Register your models here.
+
+class NotebookCategoryChoice(forms.ModelChoiceField):
+    pass
+
+class SmartphonesCategoryChoice(forms.ModelChoiceField):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Notebook)
+class NotebookAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoice(Category.objects.filter(slug_category='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(Smartphone)
+class SmartphoneAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return SmartphonesCategoryChoice(Category.objects.filter(slug_category='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BasketProduct)
+class BasketProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
--- webshop/models.py
+++ webshop/models.py
@@ -15,10 +15,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-
     class Meta:
-        abstract = True #
-
+        abstract = True  #
 
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     name_product = models.CharField(max_length=255, verbose_name='Название товара')
@@ -31,9 +29,40 @@ class Product(models.Model):
         return f'{self.category} {self.name_product} {self.price}'
 
 
+class Notebook(Product):
+    brand = models.CharField(max_length=255, verbose_name='Бренд ноутбука')
+    diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
+    display = models.CharField(max_length=255, verbose_name="Тип дисплея")
+    cpu = models.CharField(max_length=255, verbose_name="Процессор")
+    ram = models.CharField(max_length=255, verbose_name="Оперативная память")
+    video = models.CharField(max_length=255, verbose_name="Видеокарта")
+
+    def __str__(self):
+        return f'{self.category.name_category}: {self.name_product}'
+
+class Smartphone(Product):
+
+    brand = models.CharField(max_length=64, verbose_name='Бренд смартфона')
+    model_smart = models.CharField(max_length=64, verbose_name='Модель смартфона')
+    diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
+    display = models.CharField(max_length=255, verbose_name="Тип дисплея")
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum = models.CharField(max_length=255, verbose_name="Объем аккумулятора")
+    ram = models.CharField(max_length=64, verbose_name="Оперативная память")
+    sd_card = models.BooleanField(default=True, verbose_name="Возможность использования SD карты")
+    sd_card_max_size = models.CharField(max_length=255, verbose_name=" Максимальный объем карты памяти")
+    main_cam = models.CharField(max_length=64, verbose_name="Главная камера")
+    front_cam = models.CharField(max_length=64, verbose_name="Фронтальная камера")
+
+
+
+    def __str__(self):
+        return f'{self.category.name_category}: {self.name_product}'
+
 class BasketProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
-    basket = models.ForeignKey('Basket', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_basket')
+    basket = models.ForeignKey('Basket', verbose_name='Корзина', on_delete=models.CASCADE,
+                               related_name='related_basket')
     content = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     cont_object = GenericForeignKey('content', 'object_id')
@@ -59,8 +88,5 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20, verbose_name='Номер телефона')
     adress = models.CharField(max_length=255, verbose_name='Адресс проживания')
 
-
     def __str__(self):
         return f'Пользователь: {self.user.first_name} {self.user.last_name}'
-
-
